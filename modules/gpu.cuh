e4ce3d59094c918f50a8419ffe081a8617dea83c
IritaSee <iga.narendra@gmail.com>
2024-08-14 14:58:51 +0900

    add land time profiling

--- modules/gpu.cuh
+++ modules/gpu.cuh
@@ -115,7 +115,7 @@ __device__ void kernel_DoDrugSim_post(double *d_ic50, double *d_cvar, double d_c
                                         double *d_mec_CONSTANTS, double *d_mec_STATES, double *d_mec_RATES,
                                         double *d_mec_ALGEBRAIC, double *time, double *states, double *out_dt,
                                         double *cai_result, double *ina, double *inal, double *ical, double *ito,
-                                        double *ikr, double *iks, double *ik1, double *tension, double *tcurr, double *dt,
+                                        double *ikr, double *iks, double *ik1, double *tension, double *tcurr, double *dt, double *dt_mech, double *tcurr_mech, 
                                         unsigned short sample_id, unsigned int sample_size, cipa_t *temp_result,
                                         cipa_t *cipa_result, param_t *p_param);
 
